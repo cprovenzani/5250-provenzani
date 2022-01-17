a5a674e0ee48716e5c9bb6a9a1d8bb834152f3c7
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-01-16 21:49:07 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Monkey Socks", Description="Armour up!!!", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Loud Banjo", Description="Too noisy for opponents' health", Value=5 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Blue Beans", Description="Magic beans", Value=7},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Water Cannon", Description="Drown your enemies!!!", Value=9 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Cat Claws", Description="Scratch inferno", Value=6 }
             };
         }
 
